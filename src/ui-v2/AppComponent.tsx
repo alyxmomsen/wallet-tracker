f1a8b7e205c23b96be2cfdf91f985c07c9ec6656
alex <shizofranky@gmail.com>
2024-12-06 22:07:06 +0300

    'update'

--- src/ui-v2/AppComponent.tsx
+++ src/ui-v2/AppComponent.tsx
@@ -3,13 +3,11 @@ import { UseAppContext } from './context/UseAppContext'
 import PersonCardUI from './PersonCardUI'
 import AddRequirementForm from './add-req-form/AddRequirementFormWindow'
 import TrackComponentUI from './track-component-ui/TrackComponentUI'
+import MainMenuRibbon from './MainMenuRibbon/MainMenuRibbon'
 
 const AppComponent = () => {
     const { app, curPage, setCurPage, currentPerson, setCurrentPerson } =
         UseAppContext()
-
-    const [isLogined, setIsLogined] = useState(false)
-    const [isPersonMenuRolledDown, setIsPersonMenuRolledDown] = useState(false)
 
     useEffect(() => {}, [currentPerson])
     useEffect(() => {}, [curPage])
@@ -17,90 +15,7 @@ const AppComponent = () => {
     return (
         <div>
             <div className="main-window__header">
-                <button
-                    onClick={() => {
-                        if (currentPerson) {
-                            setCurPage(<PersonCardUI person={currentPerson} />)
-                            setIsLogined(false)
-                        }
-                    }}
-                    disabled={currentPerson ? false : true}
-                    className={`main-menu__button btn`}
-                >
-                    {currentPerson ? currentPerson.getName() : 'Person'}
-                </button>
-                <button
-                    onClick={() => {
-                        if (currentPerson) {
-                            setCurPage(
-                                <AddRequirementForm person={currentPerson} />
-                            )
-                        }
-                    }}
-                    disabled={currentPerson ? false : true}
-                    className={`main-menu__button btn`}
-                >
-                    Add requirement
-                </button>
-                <button
-                    onClick={() => {
-                        if (currentPerson) {
-                            setCurPage(
-                                <TrackComponentUI person={currentPerson} />
-                            )
-                        }
-                    }}
-                    disabled={currentPerson ? false : true}
-                    className={`main-menu__button btn`}
-                >
-                    Track
-                </button>
-                <button
-                    onClick={() => {
-                        if (currentPerson) {
-                            setCurrentPerson(null)
-                            setIsLogined(false)
-                            setIsPersonMenuRolledDown(false)
-                            return
-                        }
-                        // setIsLogined((cur) => !cur)
-                        setIsPersonMenuRolledDown((cur) => !cur)
-                        setCurPage(
-                            <div>
-                                <h1>Login page</h1>
-                            </div>
-                        )
-                    }}
-                    disabled={false}
-                    className={`main-menu__button btn`}
-                >
-                    {currentPerson ? 'LogOut' : 'LogIn'}
-                </button>
-                {isPersonMenuRolledDown && !currentPerson ? (
-                    <div>
-                        {[
-                            ...app
-                                .getPersons()
-                                .filter((person) => {
-                                    return !currentPerson
-                                        ? true
-                                        : currentPerson === person
-                                })
-                                .map((person) => {
-                                    return (
-                                        <button
-                                            className="btn main-menu__button"
-                                            onClick={() => {
-                                                setCurrentPerson(person)
-                                            }}
-                                        >
-                                            {person.getName()}
-                                        </button>
-                                    )
-                                }),
-                        ]}
-                    </div>
-                ) : null}
+                <MainMenuRibbon />
             </div>
             <div className="main-window__body overflow-y">{curPage}</div>
         </div>
---
+++ src/ui-v2/MainMenuRibbon/MainMenuRibbon.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react'
+import { UseAppContext } from '../context/UseAppContext'
+import PersonCardUI from '../PersonCardUI'
+import AddRequirementForm from '../add-req-form/AddRequirementFormWindow'
+import TrackComponentUI from '../track-component-ui/TrackComponentUI'
+
+const MainMenuRibbon = () => {
+    const { currentPerson, setCurrentPerson, setCurPage, app } = UseAppContext()
+
+    const [isLogined, setIsLogined] = useState(false)
+    const [isPersonMenuRolledDown, setIsPersonMenuRolledDown] = useState(false)
+
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    if (currentPerson) {
+                        setCurPage(<PersonCardUI person={currentPerson} />)
+                        setIsLogined(false)
+                    }
+                }}
+                disabled={currentPerson ? false : true}
+                className={`main-menu__button btn`}
+            >
+                {currentPerson ? currentPerson.getName() : 'Person'}
+            </button>
+            <button
+                onClick={() => {
+                    if (currentPerson) {
+                        setCurPage(
+                            <AddRequirementForm person={currentPerson} />
+                        )
+                    }
+                }}
+                disabled={currentPerson ? false : true}
+                className={`main-menu__button btn`}
+            >
+                Add requirement
+            </button>
+            <button
+                onClick={() => {
+                    if (currentPerson) {
+                        setCurPage(<TrackComponentUI person={currentPerson} />)
+                    }
+                }}
+                disabled={currentPerson ? false : true}
+                className={`main-menu__button btn`}
+            >
+                Track
+            </button>
+            <button
+                onClick={() => {
+                    if (currentPerson) {
+                        setCurrentPerson(null)
+                        setIsLogined(false)
+                        setIsPersonMenuRolledDown(false)
+                        return
+                    }
+                    // setIsLogined((cur) => !cur)
+                    setIsPersonMenuRolledDown((cur) => !cur)
+                    setCurPage(
+                        <div>
+                            <h1>Login page</h1>
+                        </div>
+                    )
+                }}
+                disabled={false}
+                className={`main-menu__button btn`}
+            >
+                {currentPerson ? 'LogOut' : 'LogIn'}
+            </button>
+            {isPersonMenuRolledDown && !currentPerson ? (
+                <div>
+                    {[
+                        ...app
+                            .getPersons()
+                            .filter((person) => {
+                                return !currentPerson
+                                    ? true
+                                    : currentPerson === person
+                            })
+                            .map((person) => {
+                                return (
+                                    <button
+                                        className="btn main-menu__button"
+                                        onClick={() => {
+                                            setCurrentPerson(person)
+                                        }}
+                                    >
+                                        {person.getName()}
+                                    </button>
+                                )
+                            }),
+                    ]}
+                </div>
+            ) : null}
+        </div>
+    )
+}
+
+export default MainMenuRibbon
